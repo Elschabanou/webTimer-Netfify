7c233f40632aab86ad7a67d8250ed06af8852708
Elschabanou <felix@schabana.de>
2024-03-12 12:49:18 +0100

    password

---
+++ public/login.js
@@ -0,0 +1,23 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get user input
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Fetch JSON data (replace this with actual fetch request)
+    fetch('user_data.json')
+        .then(response => response.json())
+        .then(data => {
+            // Check if user credentials match
+            var user = data.find(user => user.user === username && user.password === password);
+            if (user) {
+                // Redirect to home.html if credentials are correct
+                window.location.href = '/home.html';
+            } else {
+                // Display error message if credentials are incorrect
+                document.getElementById('errorMessage').textContent = 'Invalid username or password.';
+            }
+        })
+        .catch(error => console.log(error));
+});
