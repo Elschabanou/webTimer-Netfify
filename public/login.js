c38e4154e9d2f105c246bc803cfd15fc923ddc2a
Elschabanou <felix@schabana.de>
2024-03-13 00:11:18 +0100

    test

--- public/login.js
+++ public/login.js
@@ -15,6 +15,17 @@ document.getElementById('login-form').addEventListener('submit', function (event
                 // Redirect to home.html if credentials are correct
                 window.location.href = '/home.html';
                 document.getElementById('errorMessage').style.visibility = "hidden";
+                //test
+                document.getElementById("add-Event").addEventListener("click", function() {
+                    document.getElementById("popup").style.display = "block";
+                  });
+                  
+                  document.getElementById("save-event").addEventListener("click", function() {
+                    // Add your save event logic here
+                    console.log("Event saved");
+                    // Close the popup
+                    document.getElementById("popup").style.display = "none";
+                  });
 
             } else {
                 // Display error message if credentials are incorrect
