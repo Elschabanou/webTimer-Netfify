1e25bf7495813ceb589fc759a330ada8ad3003c5
Elschabanou <felix@schabana.de>
2024-03-12 12:53:10 +0100

    d

--- public/login.js
+++ public/login.js
@@ -1,4 +1,4 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
 
     // Get user input
