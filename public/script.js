9dc9b448f88fba2c583a82533d44c3ad8f87ce1b
Elschabanou <felix@schabana.de>
2024-03-13 23:58:49 +0100

    df

--- public/script.js
+++ public/script.js
@@ -124,7 +124,7 @@ document.getElementById("save-event").addEventListener("click", function () {
         description: document.querySelector("#popup textarea").value
     };
 
-    fetch('public/netlify/functions/writeNewEvent', {
+    fetch('.netlify/functions/writeNewEvent', {
         method: 'POST',
         body: JSON.stringify(eventData),
         headers: {
