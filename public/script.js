ed85bbf1ed46b0373a783826ea7704c76577bb2a
Elschabanou <felix@schabana.de>
2024-05-27 13:50:16 +0200

    removed weeks

--- public/script.js
+++ public/script.js
@@ -9,27 +9,25 @@ var x = setInterval(function () {
     // Calculate the distance between now and the countdown date
     var distance = countDownDate - now;
 
-    // Calculate weeks, days, hours, minutes, and seconds
-    var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
-    var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
+    // Calculate days, hours, minutes, and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in the elements with the corresponding IDs
-    document.getElementById("weeks").innerHTML = weeks < 10 ? "0" + weeks : weeks;
     document.getElementById("days").innerHTML = days < 10 ? "0" + days : days;
     document.getElementById("hours").innerHTML = hours < 10 ? "0" + hours : hours;
     document.getElementById("minutes").innerHTML = minutes < 10 ? "0" + minutes : minutes;
     document.getElementById("seconds").innerHTML = seconds < 10 ? "0" + seconds : seconds;
 
     // If the countdown is over, display a message
-    // optional very cool visuals
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "EXPIRED";
     }
 }, 1000);
+
 
 // Load events from JSON file
 fetch('/json/events.json')
